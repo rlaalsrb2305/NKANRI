539fdb935604c63e62443399a57e81c089c88c75
rlaalsrb2305 <51859951+rlaalsrb2305@users.noreply.github.com>
2019-06-23 02:38:23 +0900

    Update namtang.py

--- namtang.py
+++ namtang.py
@@ -102,10 +102,13 @@ async def on_message(message):
 
     if message.content.startswith("!첩보요원"):
         date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
+        now = datetime.datetime.now()
+        nowDate = now.strftime('%Y-%m-%d')
         embed = discord.Embed(color=0x00ff00)
         embed.add_field(name="이름", value=message.author.name, inline=True)
         embed.add_field(name="서버닉네임", value=message.author.display_name, inline=True)
         embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일" , inline=True)
+        embed.add_field(name="오늘자", value=nowDate , inline=True)
         embed.add_field(name="아이디", value=message.author.id, inline=True)
         embed.set_thumbnail(url=message.author.avatar_url)
         await client.send_message(message.channel, embed=embed)
